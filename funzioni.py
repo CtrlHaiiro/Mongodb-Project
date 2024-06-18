ee9f87712b4bbfaaf7aa95e0daac66452f217439
eleonora <eleonora.gonzaga@itsrizzoli.it>
2024-06-18 15:41:23 +0200

    aggiunta purchase_page, search_concert e funzioni di visualizzazione

--- funzioni.py
+++ funzioni.py
@@ -1,45 +1,135 @@
 import os
 import time
 from pymongo import MongoClient
+import re
+
+global collection
 
 
 def start_client():
+#connects do mongodb database
     try:
-        client = MongoClient('mongodb+srv://mongodb-ele.xku5epx.mongodb.net/')
-        db = client.get_database("Concerti")
         global collection
+        client = MongoClient("mongodb://localhost:27017/")
+        db = client.get_database("Concerti")
         collection = db.get_collection("concerti")
-        print(collection.name)
         return collection
     except Exception:
         print(Exception)
 
 
-def exit_program(coll):
+def view_details(event):
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("uscita dal programma")
-    time.sleep(1)
+    name = event["nome"]
+    artists = event["artisti"]
+    places = event["luogo"]
+
+    print(f"Nome: {name}\n"
+          f"Artisti : {', '.join(artists)}")
+
+    for i, place in enumerate(places):
+
+        data = place["tempo"]["data"]
+        time = place["tempo"]["ora"]
+        locale = place["locale"]
+        city = place["citta"]
+        posti = place["posti"]
+        posti_vip = posti["vip"]["numero_posti"]
+        posti_premium = posti["premium"]["numero_posti"]
+        posti_base = posti["base"]["numero_posti"]
+        posti_totali = posti_vip + posti_base + posti_premium
+
+        print(f"\n{i + 1}- Città: {city}\n"
+              f"Locale: {locale}\n"
+              f"Data: {', '.join(data)}, ore {time}\n"
+              f"{posti_totali} posti disponibili")
 
 
-def search_concert(coll):
+def purchase_page(event):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("purchase_page\n")
+    print(event["nome"])
+    concert_info = event["luogo"][0]
+    posti = concert_info["posti"]
+    print(f"posti: {posti}")
+    categorie_posti = ["vip", "premium", "base"]
+    for i, category in enumerate(categorie_posti):
+        print(f"{i+1}- posti {category}\n"
+              f"{posti[category]["numero_posti"]} posti disponibili\n")
+    choice = int(input(f"0- indietro\n"
+                   f"inserisci una scelta: "))
+    if choice == 0:
+        return search_concert()
+    num_biglietti = int(input("inserisci numero biglietti da acquistare: "))
+    print("da implementare\n")
+    time.sleep(2)
+    return search_concert()
+
+
+
+
+
+    time.sleep(2)
+    return
+
+
+def show_avariable_concerts(concerts):
+    print(f"concerti disponibili: \n"
+          f"{len(concerts)} risultati trovati\n")
+    for i, concert in enumerate(concerts):
+        print(
+            f"{i + 1} - {concert["nome"]}  ")
+    choice = int(input("\ninserisci il numero del concerto \n"
+                       "per visualizzare i dettagli ( 0 per tornare indietro ) : "))
+    if choice == 0:
+        return search_concert()
+    else:
+        return choice
+
+
+def search_concert():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("cerca per concerto")
+    query = {}
+    concert = str(input("inserisci il nome del concerto: ")).capitalize()
+    regex_pattern = re.compile(f".*{re.escape(concert)}.*", re.IGNORECASE)
+
+    query = {"nome": {"$regex": regex_pattern}}
+    results = list(collection.find(query))
+    if results:
+        choice = show_avariable_concerts(results)
+        view_details(results[choice - 1])
+        choice = int(input("\n0 - indietro\nseleziona una data: \n"))
+        if choice != 0:
+            purchase_page(results[choice - 1])
+        else: return search_concert()
+
+    else:
+        print("Nessun concerto trovato con questo nome.")
+        time.sleep(2)
+
     time.sleep(1)
 
 
-def search_artist(coll):
+def search_artist():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("cerca per artista")
     time.sleep(1)
 
 
-def search_by_date(coll):
+def search_by_date():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("cerca per intervallo di date")
     time.sleep(1)
 
 
-def search_by_distance(coll):
+def search_by_distance():
     os.system('cls' if os.name == 'nt' else 'clear')
     print("ricerca per distanza")
+    time.sleep(1)
+
+
+def exit_program():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("uscita dal programma")
     time.sleep(1)
--- main.py
+++ main.py
@@ -1,34 +1,47 @@
 import funzioni
 import os
+import time
+from pymongo import MongoClient
+import re
 
 
 def display_menu(coll):
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("Benvenuto!\n"
-              "ecco le opzioni che sono a tua disposizione:\n"
-              "1- Ricerca per Concerto\n"
-              "2- Ricerca per Artista\n"
-              "3- Ricerca Concerti vicino a te\n"
-              "4- Ricerca concerti per intervallo di date\n"
-              "5- Esci dal programma\n")
+        try:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("                Benvenuto!\n"
+                  "ecco le opzioni che sono a tua disposizione:\n\n"
+                  "| 1- Ricerca per Concerto                     |\n"
+                  "| 2- Ricerca per Artista                      |\n"
+                  "| 3- Ricerca Concerti vicino a te             |\n"
+                  "| 4- Ricerca concerti per intervallo di date  |\n"
+                  "| 0- Esci dal programma                       |\n")
 
-        choice = int(input("Inserisci la tua scelta:  "))
-        match choice:
-            case 1:
-                funzioni.search_concert(coll)
-            case 2:
-                funzioni.search_artist(coll)
-            case 3:
-                funzioni.search_by_date(coll)
-            case 4:
-                funzioni.search_by_distance(coll)
-            case 5:
-                funzioni.exit_program(coll)
-                break
+            choice = int(input("Inserisci la tua scelta:  "))
+            match choice:
+                case 1:
+                    funzioni.search_concert()
+                case 2:
+                    funzioni.search_artist()
+                case 3:
+                    funzioni.search_by_date()
+                case 4:
+                    funzioni.search_by_distance()
+                case 0:
+                    funzioni.exit_program()
+                    break
+                case _:
+                    raise ValueError
+
+        except ValueError:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("---- la scelta inserita non è valida ----")
+            time.sleep(2)
+
+        except Exception as e:
+            print(f"Errore: {e}")
 
 
 if __name__ == '__main__':
-    collection = funzioni.start_client()
-    display_menu(collection)
-
+    coll = funzioni.start_client()
+    display_menu(coll)
