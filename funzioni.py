cfd04ab895187be08846c5bdecd41693ad151eda
eleonora <eleonora.gonzaga@itsrizzoli.it>
2024-06-20 12:31:01 +0200

    purchase page aggiornamento

--- funzioni.py
+++ funzioni.py
@@ -1,4 +1,5 @@
 import os
+import random
 import time
 from pymongo import MongoClient
 import re
@@ -7,7 +8,7 @@ global collection
 
 
 def start_client():
-#connects do mongodb database
+    # connects to mongodb database
     try:
         global collection
         client = MongoClient("mongodb://localhost:27017/")
@@ -28,45 +29,50 @@ def view_details(event):
           f"Artisti : {', '.join(artists)}")
 
     for i, place in enumerate(places):
-
         data = place["tempo"]["data"]
         time = place["tempo"]["ora"]
-        locale = place["locale"]
+        local = place["locale"]
         city = place["citta"]
-        posti = place["posti"]
-        posti_vip = posti["vip"]["numero_posti"]
-        posti_premium = posti["premium"]["numero_posti"]
-        posti_base = posti["base"]["numero_posti"]
-        posti_totali = posti_vip + posti_base + posti_premium
+        seats = place["posti"]
 
         print(f"\n{i + 1}- Città: {city}\n"
-              f"Locale: {locale}\n"
+              f"Locale: {local}\n"
               f"Data: {', '.join(data)}, ore {time}\n"
-              f"{posti_totali} posti disponibili")
+              f"{seats['numero_posti']} posti disponibili\n"
+              f"prezzo: {seats['prezzo']} €")
 
 
 def purchase_page(event):
     os.system('cls' if os.name == 'nt' else 'clear')
     print("purchase_page\n")
     print(event["nome"])
-    concert_info = event["luogo"][0]
-    posti = concert_info["posti"]
-    categorie_posti = ["vip", "premium", "base"]
-    for i, category in enumerate(categorie_posti):
-        print(f"{i+1}- posti {category}\n"
-              f"{posti[category]["numero_posti"]} posti disponibili\n")
-    choice = int(input(f"0- indietro\n"
-                   f"inserisci una scelta: "))
+    seats = event["luogo"][0]["posti"]
+    name_seats= seats["posti"]
+    print(name_seats)
+
+    num_biglietti = int(input("inserisci il numero biglietti da acquistare: "))
+
+    choice = int(input(f"il prezzo totale per {num_biglietti} biglietti e': {seats['prezzo'] * num_biglietti} €\n"
+                       f"- 1 per effettuare l'acquisto\n"
+                       f"- 0 per tornare indietro\n"
+                       f"inserisci scelta: "))
     if choice == 0:
-        return search_concert()
-    num_biglietti = int(input("inserisci numero biglietti da acquistare: "))
-    print("da implementare\n")
-    time.sleep(2)
-    return search_concert()
+        return purchase_page(event)
 
-
-
-
+    elif choice == 1:
+        print("da implementare\n")
+        selected_seats = seats["posti"][:num_biglietti]
+        new_list = [x for x in name_seats if x not in selected_seats]
+        print(new_list)
+        for seat in selected_seats:
+            print(collection.find_one({"nome": event["nome"]}))
+            collection.update_one({"nome": event["nome"]},
+                                  {"$set": {f"luogo.0.posti.posti.{seat}": new_list}})
+            collection.update_one({"nome": event["nome"]},
+                                  {"$set": {f"luogo.0.posti.numero_posti": len(seats["posti"]) - num_biglietti}})
+        print("acquisto effettuato con successo")
+        time.sleep(2)
+        return
 
     time.sleep(2)
     return
@@ -100,8 +106,9 @@ def search_concert():
         view_details(results[choice - 1])
         choice = int(input("\n0 - indietro\nseleziona una data: \n"))
         if choice != 0:
-            purchase_page(results[choice - 1])
-        else: return search_concert()
+            purchase_page(results[choice])
+        else:
+            return search_concert()
 
     else:
         print("Nessun concerto trovato con questo nome.")
